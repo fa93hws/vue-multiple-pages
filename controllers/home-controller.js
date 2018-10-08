0f399971dac0551af5572e26bf168fe8d6ccd8d6
fa93hws <wjun0912@gmail.com>
2018-10-09 03:25:45 +1100

    improve cmd

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,5 +1,5 @@
 module.exports = {
   serve(req, res) {
-    res.render('pages/home/template', { author: 'fa93hws', title: 'This is the home page' })
+    res.render('pages/home/template', { author: 'fa93hws', title: 'This is the home page', mode: 'development' })
   }
 }
