5296e7ea24aeba66c220767eb59e5419bc93e858
fa93hws <wjun0912@gmail.com>
2018-11-20 08:44:26 +1100

    remove unused hmr.js, specific hmr for development only

--- scripts/webpack-config/config-producer.js
+++ scripts/webpack-config/config-producer.js
@@ -27,14 +27,16 @@ function collectFiles(targetFolder, files) {
  * @return {object} { fileName: path } 
  * @return {string} fileName: relative path to pages. such as pages/home-bundle
  */
-function transformEntries(entries) {
+function transformEntries(entries, mode = "production") {
   const output = {};
   entries.forEach(entry => {
     const segments = entry.split(path.sep);
     segments.splice(-1);
     segments.splice(0, 2);
     const fileName = segments.join(path.sep) +  '-bundle';
-    output[fileName] = [`.${path.sep}${entry}`, 'webpack-hot-middleware/client?reload=true'];
+    output[fileName] = [`.${path.sep}${entry}`];
+    if (mode === 'development')
+      output[fileName].push('webpack-hot-middleware/client?reload=true');
   })
   return output;
 }
@@ -44,9 +46,8 @@ function transformEntries(entries) {
  */
 export default function(mode) {
   let entry = collectFiles(webpackPath.views, []);
-  entry = transformEntries(entry);
+  entry = transformEntries(entry, mode);
   entry.vendors = webpackPath.vendorDependencies;
-  if (mode === 'development') entry['hmr'] = 'webpack-hot-middleware/client?reload=true';
   const webpackConfig = {
     ...baseConfig,
     mode,
